83f94e939c63d2e33ae5836774d9af1ef910ee6a
abdout <osmanabdout@hotmail.com>
2025-08-30 06:02:01 +0300

    j

--- src/components/site/vedio-card.tsx
+++ src/components/site/vedio-card.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
 
-interface VideoCardProps {
+export interface VideoCardProps {
   logo: string
   title: string
   description: string
